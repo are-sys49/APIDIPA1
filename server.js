f3dba6383df49b1df5ebf8950745bc45824de0a9
are-sys49 <cabrerarely24@gmail.com>
2025-07-29 13:19:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
 const jwt = require("jsonwebtoken");
 
